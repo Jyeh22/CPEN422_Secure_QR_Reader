af2c55df903146c0927bf3db15b87d2cc41a2fc3
Jyeh22 <Justin.yeh@shaw.ca>
2022-12-05 20:30:29 -0800

    changed variable names, commented out test line

--- Server/QRdata_analyzer.js
+++ Server/QRdata_analyzer.js
@@ -2,10 +2,10 @@ const {PythonShell} = require('python-shell');
 const { text } = require('body-parser');
 
 
-var analyzeQR = function(url){
+var analyzeQR = function(data){
     //Replace test.py with python script to run
     let pyshell = new PythonShell('test.py');
-    pyshell.send(url);
+    pyshell.send(data);
 
     pyshell.on('message', (message)=>{
        return message;
@@ -17,6 +17,6 @@ var analyzeQR = function(url){
       });
 }
 
-analyzeQR("test");
+//analyzeQR("test");
 
 module.exports = analyzeQR;
